bd37ebf4de819b9a4bd140c0fca5507d90d05f18
rajbhirad-0909 <rajbhirad@gmail.com>
2025-11-25 00:42:14 +0530

    Added test automation script

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+# Activate virtual environment
+source venv/Scripts/activate
+
+# Run tests
+pytest
+
+# Return exit code based on pytest result
+if [ $? -eq 0 ]; then
+    echo "All tests passed successfully."
+    exit 0
+else
+    echo "Some tests failed!"
+    exit 1
+fi
+
 
